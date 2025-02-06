e357eb39959dbfec1f2223b0ead468c7343585fb
Sam Sepiol <jakubsopel7@gmail.com>
2025-02-07 00:34:43 +0100

    basic terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_ecr_repository" "minecraft-tool" {
+  name                 = "minecraft-tool"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
